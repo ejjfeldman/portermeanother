b644cd5c0eb957dd51f4647457eb1316fc85608b
Erin Feldman <ejjfeldman@gmail.com>
2017-08-24 15:02:14 -0400

    new changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -202,6 +202,33 @@ class App extends Component {
       <div className="foam-5"></div>
       <div className="foam-6"></div>
       <div className="foam-7"></div>
+      <div className="foam-8"></div>
+      <div className="foam-9"></div>
+      <div className="foam-10"></div>
+      <div className="foam-11"></div>
+      <div className="foam-12"></div>
+      <div className="foam-13"></div>
+      <div className="foam-14"></div>
+      <div className="foam-15"></div>
+       <div className="foam-16"></div>
+       <div className="foam-17"></div>
+       <div className="foam-18"></div>
+       <div className="foam-19"></div>
+       <div className="foam-20"></div>
+       <div className="foam-21"></div>
+       <div className="foam-22"></div>
+       <div className="foam-23"></div>
+       <div className="foam-24"></div>
+       <div className="foam-25"></div>
+       <div className="foam-26"></div>
+       <div className="foam-27"></div>
+       <div className="foam-28"></div>
+       <div className="foam-29"></div>
+       <div className="foam-30"></div>
+       <div className="foam-31"></div>
+       <div className="foam-32"></div>
+       <div className="foam-33"></div>
+       <div className="foam-34"></div>
     </div>
     
     <div id="liquid">
@@ -211,14 +238,32 @@ class App extends Component {
       <div className="bubble bubble3"></div>
       <div className="bubble bubble4"></div>
       <div className="bubble bubble5"></div>
+      <div className="bubble bubble6"></div>
+      <div className="bubble bubble7"></div>
+      <div className="bubble bubble8"></div>
+      <div className="bubble bubble9"></div>
+      <div className="bubble bubble10"></div>
+            <div className="bubble bubble11"></div>
+      <div className="bubble bubble12"></div>
+      <div className="bubble bubble13"></div>
+      <div className="bubble bubble14"></div>
+      <div className="bubble bubble15"></div>
+            <div className="bubble bubble16"></div>
+      <div className="bubble bubble17"></div>
+      <div className="bubble bubble18"></div>
+      <div className="bubble bubble19"></div>
+      <div className="bubble bubble20"></div>
+      <div className="bubble bubble21"></div>
+      <div className="bubble bubble22"></div>
     </div>
-  </div>
-</div>
-  
-        <div className="App-header jumbotron">
+    <div className="App-header jumbotron" id="header">
           <h1>Porter Me Another</h1>
           <p>Don't Wine, Have a Beer</p>
         </div>
+  </div>
+</div>
+  
+
 
         <div className="mainContent">
           {React.cloneElement(this.props.children, {
